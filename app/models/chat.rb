e56772f83e2714fe472e8e79d9c05e0761da2d74
michele di stadio <distadiomichele93@gmail.com>
2024-12-13 21:25:24 +0100

    chat logic

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -1,5 +1,6 @@
 class Chat < ApplicationRecord
   belongs_to :user
   belongs_to :profile
-  has_many :messages
+  has_many :messages, dependent: :destroy
+  scope :involving, ->(user) { where(user: user).or(where(profile: user.profile)) }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "pages#home"
+  root to: "chats#index"
 
-  resources :profiles, only: [:index, :show, :new, :create]
+  resources :profiles, only: [:index, :show, :new, :create] do
+    resources :chats, only: [:index, :new, :create]
+  end
+
+  resources :chats, only: [:show] do
+    resources :messages, only: [:create]
+  end
 
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
