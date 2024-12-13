4afe9bed56842eafaaf79af38b77b029e028b1b1
michele di stadio <distadiomichele93@gmail.com>
2024-12-13 18:51:28 +0100

    logic profile

---
+++ test/controllers/profile_controller_test.rb
@@ -0,0 +1,18 @@
+require "test_helper"
+
+class ProfileControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get profile_index_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get profile_show_url
+    assert_response :success
+  end
+
+  test "should get create" do
+    get profile_create_url
+    assert_response :success
+  end
+end
