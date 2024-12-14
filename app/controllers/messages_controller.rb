115727d1115e80c0006d8a154e227b841239848f
michele di stadio <distadiomichele93@gmail.com>
2024-12-14 15:24:04 +0100

    last changes

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,17 +1,23 @@
 class MessagesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_chat
+
   def create
-    @chat = Chat.find(params[:chat_id])
-    @message = Message.new(message_params)
-    @message.chat = @chat
+    @message = @chat.messages.build(message_params)
     @message.user = current_user
+
     if @message.save
-      redirect_to chat_path(@chat)
+      redirect_to chat_path(@chat), notice: 'Message was successfully sent.'
     else
-      render "chat/show", status: :unprocessable_entity
+      redirect_to chat_path(@chat), alert: 'Failed to send message.'
     end
   end
 
   private
+
+  def set_chat
+    @chat = Chat.find(params[:chat_id])
+  end
 
   def message_params
     params.require(:message).permit(:content)
